5b280f7fcc41659d65044b49d357cf8f8509738a
leedawnn <2g_hee@naver.com>
2022-08-29 21:34:11 +0900

    feat: test PR

--- daengder/pages/_app.js
+++ daengder/pages/_app.js
@@ -1,9 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
 
 // test pull request
+// test test PR ~~!!!
