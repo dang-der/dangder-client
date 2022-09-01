211dd7f312836077138663f791dff261236d8715
hyejineee <dev.hyejin04@gmail.com>
2022-09-02 07:27:41 +0900

    220902 - [feat] : 로그인 페이지 퍼블리싱

---
+++ src/Auth/Login/Login.styles.ts
@@ -0,0 +1,63 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+export const LogoImage = styled.img`
+  width: 80%;
+  padding: 1rem;
+  margin-bottom: 5rem;
+`;
+
+export const InputErrorWrapper = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+  span {
+    color: red;
+    font-size: 0.75rem;
+    padding-left: 1rem;
+  }
+`;
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  height: 3.6rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0.5rem 1.5rem;
+  background-color: #fafafa;
+  border-radius: 1rem;
+
+  input {
+    width: 90%;
+    border: none;
+    background-color: transparent;
+  }
+`;
+
+export const LoginButton = styled.button`
+  width: 80%;
+  height: 4.5rem;
+  border: none;
+  border-radius: 100px;
+  margin: 3rem 0;
+`;
+
+export const JoinWrapper = styled.span`
+  margin-top: 1rem;
+  font-size: 0.9rem;
+  color: #d9d9d9;
+  u {
+    color: black;
+    cursor: pointer;
+  }
+`;
