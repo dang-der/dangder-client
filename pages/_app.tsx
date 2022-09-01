2a36ee47842a913800b3faf5117efba220f7e711
hyejineee <dev.hyejin04@gmail.com>
2022-09-02 03:44:29 +0900

    220902 - [chore] : ant design 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import {Global} from "@emotion/react"
 import {globalStyles} from "../styles/GlobalStyles"
+import "antd/dist/antd.css"; 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
