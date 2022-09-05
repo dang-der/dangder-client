f0b6fb70967dc1bfc47a82ef5bda4af2b2742179
leedawnn <2g_hee@naver.com>
2022-09-06 02:23:19 +0900

    220906 - refactor:  메인 페이지 리팩토링
    react-tinder-card 사용해서 스와이프 구현, 좋아요/싫어요 버튼 삭제.
    Geolocation API 사용해서 사용자 위치 가져오기

--- src/Components/Units/Main/DogMainPage.container.tsx
+++ src/Components/Units/Main/DogMainPage.container.tsx
@@ -1,96 +1,50 @@
-import React, { useMemo, useRef, useState } from "react";
 import DogMainPageUI from "./DogMainPage.presenter";
-
-interface DogMainPageProps {
-  dogList: any;
-}
 
 // 강아지 정보 api
 const dogList = [
   {
-    name: "kkimi",
+    name: "끼미",
     age: 1,
     distance: 2,
-    play: "공놀이를 좋아하는",
+    play: "공놀이를 좋아하는 댕댕이입니다.",
     url: "/dog1.jpg",
   },
   {
-    name: "kkimi",
+    name: "오전이",
     age: 1,
     distance: 2,
-    play: "공놀이를 좋아하는",
+    play: "공놀이를 좋아하는 댕댕이입니다.",
     url: "/dog1.jpg",
   },
   {
-    name: "kkimi",
+    name: "철수",
     age: 1,
     distance: 2,
-    play: "공놀이를 좋아하는",
+    play: "공놀이를 좋아하는 댕댕이입니다.",
     url: "/dog1.jpg",
   },
 ];
 
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        alert(position.coords.latitude + " " + position.coords.longitude);
+      },
+      (error) => {
+        console.log(error);
+      },
+      {
+        enableHighAccuracy: false,
+        maximumAge: 0,
+        timeout: Infinity,
+      }
+    );
+  } else {
+    alert("GPS를 지원하지 않습니다.");
+  }
+};
+
 export default function DogMainPage() {
-  const [currentIndex, setCurrentIndex] = useState(dogList.length - 1);
-  const [lastDirection, setLastDirection] = useState();
-  const currentIndexRef = useRef(currentIndex);
-
-  console.log("currentIndex", currentIndex);
-  console.log("currentIndexRef", currentIndexRef.current);
-  const childRefs = useMemo(
-    () =>
-      Array(dogList.length)
-        .fill(0)
-        .map((i) => React.createRef()),
-    []
-  );
-
-  const updateCurrentIndex = (val: any) => {
-    console.log("updateCurrentIndex", val);
-    setCurrentIndex(val);
-    currentIndexRef.current = val;
-  };
-
-  const canSwipe = currentIndex >= 0;
-
-  const canGoBack = currentIndex < dogList.length - 1;
-
-  // const onSwipe = (direction: any) => {
-  //   console.log(`You swiped: ${direction}`);
-
-  //   if (dogList.length < currentIndex) return;
-  //   setCurrentIndex((prev) => prev + 1);
-  // };
-
-  const swiped = (direction, index) => {
-    console.log("swiped", index);
-    setLastDirection(direction);
-    updateCurrentIndex(index - 1);
-  };
-
-  const outOfFrame = (name: any, index: any) => {
-    console.log(`${name} (${index}) left the screen!`, currentIndexRef.current);
-    currentIndexRef.current >= index && childRefs[index].current.restoreCard();
-  };
-
-  const swipe = async (dir) => {
-    if (canSwipe && currentIndex < dogList.length) {
-      await childRefs[currentIndex].current.swipe(dir);
-    }
-  };
-
-  return (
-    <DogMainPageUI
-      dogList={dogList}
-      currentIndex={currentIndex}
-      lastDirection={lastDirection}
-      currentIndexRef={currentIndexRef}
-      canSwipe={canSwipe}
-      canGoBack={canGoBack}
-      swiped={swiped}
-      outOfFrame={outOfFrame}
-      swipe={swipe}
-      childRefs={childRefs}
-    />
-  );
+  return <DogMainPageUI dogList={dogList} getLocation={getLocation} />;
 }
