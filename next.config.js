81ee4a76b8270d1416826fd90253fe14cfd5af30
leedawnn <2g_hee@naver.com>
2022-09-06 20:43:23 +0900

    220906 - chore: @svgr/webpack를 위한 next.config.js 설정

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
     "/test": { page: "/test" },
     "/404": { page: "/404" },
   }),
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
