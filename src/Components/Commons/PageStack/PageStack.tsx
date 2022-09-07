a26e3da1002a5898634ca8f6da772905fc37f5b3
hyejineee <dev.hyejin04@gmail.com>
2022-09-07 11:26:17 +0900

    :fire: 220907 - [refactor] PageStack, PageController 리팩토링

--- src/Components/Commons/PageStack/PageStack.tsx
+++ src/Components/Commons/PageStack/PageStack.tsx
@@ -1,34 +1,12 @@
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { ReactNode } from "react";
 
 interface PageStackProps {
   children: ReactNode[];
-  controller: any;
+  currentPageIndex: number;
 }
-export default function PageStack({ children, controller }: PageStackProps) {
-  const sizeRef = useRef(children.length - 1);
-  const pageIndexRef = useRef(0);
-  const [currentPageIndex, setCurrentPageIndex] = useState(
-    pageIndexRef.current
-  );
-
-  useEffect(() => {
-    controller.current.next = nextPage;
-    controller.current.prev = prevPage;
-  }, []);
-
-  const nextPage = () => {
-    if (pageIndexRef.current >= sizeRef.current) return;
-
-    pageIndexRef.current += 1;
-    setCurrentPageIndex((p) => p + 1);
-  };
-
-  const prevPage = () => {
-    if (pageIndexRef.current <= 0) return;
-
-    pageIndexRef.current -= 1;
-    setCurrentPageIndex((p) => p - 1);
-  };
-
-  return <>{children?.[currentPageIndex]}</>;
+export default function PageStack({
+  children,
+  currentPageIndex,
+}: PageStackProps) {
+  return <>{children[currentPageIndex]}</>;
 }
