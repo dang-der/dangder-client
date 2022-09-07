e1148607c8c451d50a7f5f0adf3998b9e3ff7510
leedawnn <2g_hee@naver.com>
2022-09-07 16:00:57 +0900

    220907 - feat: layout 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import { AppProps } from "next/app";
-import {Global} from "@emotion/react"
-import {globalStyles} from "../styles/GlobalStyles"
-import "antd/dist/antd.css"; 
+import { Global } from "@emotion/react";
+import { globalStyles } from "../styles/GlobalStyles";
+import "antd/dist/antd.css";
 import { RecoilRoot } from "recoil";
-
+import Layout from "../src/Components/Commons/layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <RecoilRoot>
         <Global styles={globalStyles} />
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </RecoilRoot>
     </>
   );
---
+++ src/Components/Commons/layout/logo_header/index.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+export default function LogoHeader() {
+  return (
+    <Wrapper>
+      <Logo src="/logo.svg" />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 3.1rem;
+  padding: 2.5rem 2.5rem 1.25rem 2.5rem;
+`;
+
+const Logo = styled.img`
+  width: 9.875rem;
+`;
