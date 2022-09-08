59dd476ce2ad93a47fde76bb5cdbdf57ca3358e8
leedawnn <2g_hee@naver.com>
2022-09-08 16:42:39 +0900

    :fire: 220908 - feat: 레이아웃 분기 처리

--- src/Components/Commons/layout/index.tsx
+++ src/Components/Commons/layout/index.tsx
@@ -1,19 +1,44 @@
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
 import LogoHeader from "./logo_header";
 import Navigation from "./navigation";
 import PageHeader from "./page_header";
 
+const SHOW_LOGO_HEADERS = ["/", "/[dogId]"];
+
+// TODO: 채팅방 - 약속 설정에 페이지 타이틀 헤더 추가
+const SHOW_PAGE_HEADERS = [
+  "/auth/password-reset",
+  "/auth/signup",
+  "/today",
+  "/chat/[roomId]",
+  "/chat",
+  "/profile",
+  "/profile/edit",
+  "/profile/init",
+  "/settings",
+];
+
+const SHOW_NAVIGATION = ["/", "/[dogId]", "/today", "/chat", "/profile"];
+
 interface ILayoutProps {
   children: ReactNode;
 }
 
+// TODO: 불필요한 리렌더링 막기
 export default function Layout(props: ILayoutProps) {
+  const router = useRouter();
+  console.log(router);
+  const isShowLogoHeader = SHOW_LOGO_HEADERS.includes(router.pathname);
+  const isShowPageHeader = SHOW_PAGE_HEADERS.includes(router.pathname);
+  const isShowNavigation = SHOW_NAVIGATION.includes(router.pathname);
+
   return (
     <>
-      <LogoHeader />
-      {/* <PageHeader title="오늘의 댕댕이" /> */}
+      {isShowLogoHeader && <LogoHeader />}
+      {isShowPageHeader && <PageHeader />}
       <div>{props.children}</div>
-      <Navigation />
+      {isShowNavigation && <Navigation />}
     </>
   );
 }
