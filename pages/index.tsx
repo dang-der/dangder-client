42522e82b9ebae7c906439e772898af6cba540c7
leedawnn <2g_hee@naver.com>
2022-09-16 17:02:59 +0900

    :fire: 220916 - feat: 메인 페이지 스와이프 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,135 @@
-import DogMainPage from "../src/Components/Units/Main/DogMainPage.container";
+import { Stack } from "../src/Components/Units/test/DogMainPage.container.stack";
+import styled from "@emotion/styled";
+import { gql, useQuery } from "@apollo/client";
+import {
+  IDog,
+  IQuery,
+  IQueryFetchAroundDogsArgs,
+} from "../src/Commons/Types/Generated/types";
+import { useEffect, useState } from "react";
 
-export default function Home() {
-  return <DogMainPage />;
+// 로그인 유저 정보 조회
+const FETCH_LOGIN_USER = gql`
+  query fetchLoginUser {
+    fetchLoginUser {
+      id
+      dog {
+        id
+      }
+    }
+  }
+`;
+
+// 주변 강아지 정보 조회
+const FETCH_AROUND_DOGS = gql`
+  query fetchAroundDogs($id: String!, $page: Float!) {
+    fetchAroundDogs(id: $id, page: $page) {
+      id
+      name
+      age
+      description
+      img {
+        img
+      }
+    }
+  }
+`;
+
+export default function Main() {
+  const [aroundDogsList, setAroundDogsList] = useState<IDog[] | undefined>();
+  const [startPage, setStartPage] = useState(1);
+  const [activePage, setActivePage] = useState(1);
+
+  const { data: LoginUserData } =
+    useQuery<Pick<IQuery, "fetchLoginUser">>(FETCH_LOGIN_USER);
+
+  const userId = String(LoginUserData?.fetchLoginUser.id);
+  const MyDogId = String(LoginUserData?.fetchLoginUser.dog.id);
+
+  const { data: AroundDogsData, refetch } = useQuery<
+    Pick<IQuery, "fetchAroundDogs">,
+    IQueryFetchAroundDogsArgs
+  >(FETCH_AROUND_DOGS, {
+    variables: { id: MyDogId, page: 1 },
+  });
+
+  useEffect(() => {
+    if (!AroundDogsData?.fetchAroundDogs) return;
+    setAroundDogsList(AroundDogsData?.fetchAroundDogs);
+    console.log("mount: ", AroundDogsData?.fetchAroundDogs);
+  }, []);
+
+  useEffect(() => {
+    if (!AroundDogsData?.fetchAroundDogs) return;
+    setAroundDogsList(AroundDogsData?.fetchAroundDogs);
+    console.log("change: ", AroundDogsData?.fetchAroundDogs);
+  }, [AroundDogsData]);
+
+  return (
+    <Wrapper>
+      <DogCardWrapper onVote={(item, vote) => console.log(item.props, vote)}>
+        {(AroundDogsData?.fetchAroundDogs || []).map((el: IDog, i) => (
+          <Item key={el.id} className="item">
+            <DogProfile src="/dog1.jpg" />
+            <DogName>{el.name}</DogName>
+            <DogAge>{el.age}</DogAge>
+            <DogDescription>{el.description}</DogDescription>
+          </Item>
+        ))}
+      </DogCardWrapper>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const DogCardWrapper = styled(Stack)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: center;
+  width: 100%;
+  height: 100%;
+  flex-grow: 1;
+`;
+
+const Item = styled.div`
+  background-color: red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: calc(576px - 4rem);
+  max-width: 576px;
+  height: calc(100vh - 17rem);
+  @media screen and (max-width: 576px) {
+    width: calc(100vw - 4rem);
+  }
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  border-radius: 8px;
+`;
+
+const DogProfile = styled.img`
+  width: 100%;
+  height: 100%;
+  background-color: rebeccapurple;
+`;
+
+export const DogName = styled.span`
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin-right: 3px;
+`;
+
+export const DogAge = styled.span`
+  font-size: 2.5rem;
+  font-weight: bold;
+`;
+
+export const DogDescription = styled.span`
+  font-size: 1rem;
+  font-weight: 500;
+`;
---
+++ src/Components/Units/Main/DogMainPage.container.card.tsx
@@ -0,0 +1,90 @@
+import { useRef, useEffect, useState } from "react";
+import { motion, useMotionValue, useAnimation } from "framer-motion";
+import styled from "@emotion/styled";
+
+const StyledCard = styled(motion.div)`
+  position: absolute;
+`;
+
+export const Card = ({ children, onVote, ...props }) => {
+  const cardElem = useRef(null);
+
+  const x = useMotionValue(0);
+  const controls = useAnimation();
+
+  const [constrained, setConstrained] = useState(true);
+
+  const [direction, setDirection] = useState("");
+
+  const [velocity, setVelocity] = useState(0);
+
+  const getVote = (childNode: Element, parentNode: Element) => {
+    const childRect = childNode?.getBoundingClientRect();
+    const parentRect = parentNode?.getBoundingClientRect();
+    const result =
+      parentRect.left >= childRect.right
+        ? false
+        : parentRect.right <= childRect.left
+        ? true
+        : undefined;
+    return result;
+  };
+
+  const getDirection = () => {
+    return velocity >= 1 ? "right" : velocity <= -1 ? "left" : undefined;
+  };
+
+  const getTrajectory = () => {
+    setVelocity(x.getVelocity());
+    setDirection(getDirection());
+  };
+
+  const flyAway = (min: any) => {
+    const flyAwayDistance = (direction: string) => {
+      const parentWidth =
+        cardElem.current?.parentNode.getBoundingClientRect().width;
+      const childWidth = cardElem.current?.getBoundingClientRect().width;
+      return direction === "left"
+        ? -parentWidth / 2 - childWidth / 2
+        : parentWidth / 2 + childWidth / 2;
+    };
+
+    if (direction && Math.abs(velocity) > min) {
+      setConstrained(false);
+      controls.start({
+        x: flyAwayDistance(direction),
+      });
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribeX = x.onChange(() => {
+      if (cardElem.current) {
+        const childNode = cardElem.current;
+        const parentNode = cardElem.current.parentNode;
+        const result = getVote(childNode, parentNode);
+        result !== undefined && onVote(result);
+      }
+    });
+
+    return () => unsubscribeX();
+  });
+
+  return (
+    <StyledCard
+      animate={controls}
+      dragConstraints={constrained && { left: 0, right: 0, top: 0, bottom: 0 }}
+      dragElastic={1}
+      ref={(e) => {
+        cardElem.current = e;
+      }}
+      style={{ x }}
+      onDrag={getTrajectory}
+      onDragEnd={() => flyAway(500)}
+      whileTap={{ scale: 1.1 }}
+      {...props}
+    >
+      {children}
+    </StyledCard>
+  );
+};
