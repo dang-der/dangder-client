837d5ca9e3c891a4cf0810da8d0de439ab0339c8
hyejineee <dev.hyejin04@gmail.com>
2022-09-17 10:05:20 +0900

    :fire: 220917 - add 회원가입 인증번호 입력시 타이머 작동 기능 추가

--- src/Components/Units/Auth/SingUp/Page/Page.styles.ts
+++ src/Components/Units/Auth/SingUp/Page/Page.styles.ts
@@ -11,19 +11,20 @@ export const Wrapper = styled.div`
 export const GuidanceWrapper = styled.h1`
   font-weight: 700;
   font-size: 2.1rem;
+  margin-bottom: 4rem;
 `;
 
 export const AuthCodeBoxWrapper = styled.div`
   width: 100%;
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
 `;
 
 export const AuthCodeBox = styled.input`
   background-color: #f5f5f5;
   border-radius: 0.9rem;
-  height: 5.8rem;
-  width: 4.3rem;
+  height: 7rem;
+  width: 5.5rem;
   border: none;
   text-align: center;
   font-size: 1.8rem;
