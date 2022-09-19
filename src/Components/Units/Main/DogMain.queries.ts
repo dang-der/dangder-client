4ed9945cd1f681713e2a14f28f185581a1d908ad
leedawnn <2g_hee@naver.com>
2022-09-19 22:31:04 +0900

    :fire: 220919 - feat: 메인 페이지 api 연결

--- src/Components/Units/Main/DogMain.queries.ts
+++ src/Components/Units/Main/DogMain.queries.ts
@@ -6,11 +6,37 @@ export const FETCH_AROUND_DOG = gql`
       id
       name
       age
+      description
       gender
       img {
         id
         img
       }
     }
+  }
+`;
+
+export const FETCH_DOGS_DISTANCE = gql`
+  query fetchDogsDistance($id: String!) {
+    fetchDogsDistance(id: $id) {
+      dogId
+      distance
+    }
+  }
+`;
+
+export const CREATE_LIKE = gql`
+  mutation createLike($createLikeInput: createLikeInput!) {
+    createLike(createLikeInput: $createLikeInput) {
+      isMatch
+      sendId
+      receiveId
+    }
+  }
+`;
+
+export const FETCH_LOGIN_USER_IS_CERT = gql`
+  query fetchLoginUserIsCert {
+    fetchLoginUserIsCert
   }
 `;
