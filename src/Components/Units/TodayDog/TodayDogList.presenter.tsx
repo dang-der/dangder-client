149fae8be750b8ce9d8e61d75e0bdc324d846849
kwonminchul <kmc3853@gmail.com>
2022-09-20 23:17:35 +0900

    :fire: 220920 - style : 오늘의 댕댕이 CSS 수정

--- src/Components/Units/TodayDog/TodayDogList.presenter.tsx
+++ src/Components/Units/TodayDog/TodayDogList.presenter.tsx
@@ -33,39 +33,59 @@ export default function TodayDogListUI({
   return (
     <S.Wrapper>
       {todayDogData ? (
-        todayDogData?.fetchTodayDog.map((e: ITodayLikeDogOutput) => (
-          <S.ListWrapper key={e.id}>
-            <S.ListImageWrapper id={e.id} onClick={onClickMoveDogDetail(e.id)}>
-              <S.GradientBox
-              // style={{
-              //   backgroundImage: `url(${
-              //     "https://storage.googleapis.com/" +
-              //       todayDogData?.fetchTodayDog.mainImg[0] || ""
-              //   })`,
-              // }}
-              />
-              <S.ListImage
-                style={{
-                  backgroundImage: `url(${
-                    "https://storage.googleapis.com/" + e.mainImg[0] || ""
-                  })`,
-                }}
-              />
-              {/* <S.ListImage src={todayDogData?.fetchTodayDog.mainImg}></S.ListImage> */}
-            </S.ListImageWrapper>
-            <S.ListFunctionIconWrapper>
-              <S.ListFunctionMoveChat
-                onClick={onClickPass}
-                // src={todayDogData?.fetchTodayDog.mainImg}
-                src="/passIcon.png"
-              />
-            </S.ListFunctionIconWrapper>
-            <S.ListInfo>
-              <S.ListName>{e.name},</S.ListName>
-              <S.ListAge>{e.age}</S.ListAge>
-            </S.ListInfo>
-          </S.ListWrapper>
-        ))
+        todayDogData?.fetchTodayDog.map((e: ITodayLikeDogOutput) => {
+          console.log("todayDog", e);
+          return (
+            <S.ListWrapper key={e.id}>
+              <S.ListImageWrapper
+                id={e.id}
+                onClick={onClickMoveDogDetail(e.id)}
+              >
+                <S.GradientBox
+                  imageUrl={"https://storage.googleapis.com/" + e.mainImg || ""}
+                  // style={{
+                  //   backgroundImage: `url(${
+                  //     "https://storage.googleapis.com/" +
+                  //       todayDogData?.fetchTodayDog.mainImg[0] || ""
+                  //   })`,
+                  // }}
+                />
+                {/* <S.ListImage
+                  style={{
+                    backgroundPosition: "center center",
+                    backgroundSize: "cover",
+                    backgroundImage: `url(${
+                      "https://storage.googleapis.com/" + e.mainImg || ""
+                    })`,
+                  }}
+                /> */}
+                {/* <S.ListImage src={todayDogData?.fetchTodayDog.mainImg}></S.ListImage> */}
+                <S.ListFunctionIconWrapper>
+                  <S.ListFunctionMoveChat
+                    onClick={onClickPass}
+                    // src={todayDogData?.fetchTodayDog.mainImg}
+                    src="/passIcon.png"
+                  />
+                </S.ListFunctionIconWrapper>
+                <S.ListInfo>
+                  <S.ListName>{e.name},</S.ListName>
+                  <S.ListAge>{e.age}</S.ListAge>
+                </S.ListInfo>
+              </S.ListImageWrapper>
+              {/* <S.ListFunctionIconWrapper>
+                <S.ListFunctionMoveChat
+                  onClick={onClickPass}
+                  // src={todayDogData?.fetchTodayDog.mainImg}
+                  src="/passIcon.png"
+                />
+              </S.ListFunctionIconWrapper>
+              <S.ListInfo>
+                <S.ListName>{e.name},</S.ListName>
+                <S.ListAge>{e.age}</S.ListAge>
+              </S.ListInfo> */}
+            </S.ListWrapper>
+          );
+        })
       ) : (
         <></>
       )}
