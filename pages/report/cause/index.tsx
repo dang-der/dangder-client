f89ff67d39ad99a69f8bb26866fd4b4575abd219
hyejineee <dev.hyejin04@gmail.com>
2022-11-02 17:48:34 +0900

    :fire: 221101 - merge : report 불필요한 머지 삭제

--- pages/report/cause/index.tsx
+++
@@ -1,5 +0,0 @@
-import DogReportCause from "../../../src/Components/Units/Report/reportCause/DogReportCause.container";
-
-export default function ReportCausePage() {
-  return <DogReportCause />;
-}
--- pages/report/success/index.tsx
+++
@@ -1,5 +0,0 @@
-import DogReportSuccess from "../../../src/Components/Units/Report/reportSuccess/DogReportSuccess.container";
-
-export default function ReportSuccessPage() {
-  return <DogReportSuccess />;
-}
