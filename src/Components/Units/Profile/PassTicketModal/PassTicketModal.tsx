d40c1451aacc0c26caee7dfb3a9b988e744d0ee9
hyejineee <dev.hyejin04@gmail.com>
2022-11-08 15:28:21 +0900

    :fire: 221108 - feat : 마이 댕댕이 페이지 패스 티켓 모달 디자인

--- src/Components/Units/Profile/PassTicketModal/PassTicketModal.tsx
+++ src/Components/Units/Profile/PassTicketModal/PassTicketModal.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import { DimWrapper } from "../../../Commons/Modal/CustomLayoutModal/CustomLayoutModal";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
@@ -6,13 +5,31 @@ import * as S from "./PassTicketModal.styles";
 import { useRecoilState } from "recoil";
 import { PassTickModalVisible } from "../../../../Commons/Store/Modal/ModalVisibleState";
 import styled from "@emotion/styled";
+import { useQuery } from "@apollo/client";
+import { FETCH_PASS_TICKET } from "../DogProfilePage.queries";
+import {
+  IQuery,
+  IQueryFetchPassTicketArgs,
+} from "../../../../Commons/Types/Generated/types";
+import { userInfoState } from "../../../../Commons/Store/Auth/UserInfoState";
+import moment from "moment";
 
 const DeepDimWrapper = styled(DimWrapper)`
   background-color: #000000e5;
 `;
 
 export default function PassTickModal() {
-  const [visible, setVisible] = useRecoilState(PassTickModalVisible);
+  const [, setVisible] = useRecoilState(PassTickModalVisible);
+  const [userInfo] = useRecoilState(userInfoState);
+
+  const { data: fetchPassTicketData } = useQuery<
+    Pick<IQuery, "fetchPassTicket">,
+    IQueryFetchPassTicketArgs
+  >(FETCH_PASS_TICKET, {
+    variables: {
+      email: userInfo?.user?.email || "",
+    },
+  });
 
   const onClickClose = () => {
     setVisible(false);
@@ -25,7 +42,34 @@ export default function PassTickModal() {
           <CloseRoundedIcon />
         </S.CloseIconWrapper>
         <S.Wrapper>
-          <S.DogImageWrapper src={"/passticket.png"} />
+          <S.InfoWrapper>
+            <S.TextWrapper>EMAIL</S.TextWrapper>
+            <S.TextWrapper>{userInfo?.user?.email || ""}</S.TextWrapper>
+
+            <S.TextWrapper>NAME</S.TextWrapper>
+            <S.TextWrapper>{userInfo?.dog?.name}</S.TextWrapper>
+
+            <S.TextWrapper>AGE</S.TextWrapper>
+            <S.TextWrapper>{userInfo?.dog?.age}</S.TextWrapper>
+
+            <S.TextWrapper>PERIOD OF USE</S.TextWrapper>
+            <S.TextWrapper>
+              {moment(fetchPassTicketData?.fetchPassTicket.createdAt).format(
+                "YYYY.MM.DD"
+              ) +
+                " ~ " +
+                moment(fetchPassTicketData?.fetchPassTicket.expiredAt).format(
+                  "YYYY.MM.DD"
+                )}
+            </S.TextWrapper>
+          </S.InfoWrapper>
+          <S.TextWrapper style={{ marginTop: "2rem" }}>
+            WELCOME! THANK YOU TO THE
+            <br /> SPECIAL MEMBER OF DANGDER
+          </S.TextWrapper>
+          <S.TextWrapper style={{ marginTop: "2rem" }}>
+            DANGDER PASS
+          </S.TextWrapper>
         </S.Wrapper>
       </DeepDimWrapper>
     </>
